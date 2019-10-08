4e68ee7bd002f6f48a75c42f1944917d5a2cf0b0
Philip Thomas <sent2philip@gmail.com>
2019-10-08 23:06:23 +0530

    source files initial load

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# sonoff_registered_devices
-List all registered devices using eWelink
+# Sonoff Registered Devices
+
+This script lets you view all devices registered to your eWelink account using the coolkit api. The script lists the api-key for the user as well as the deviceID for each registered devices. This information is required if you want to flash stock firmware back onto a sonoff device after it has been flashed with tasmota or any custom firmwares.
+
+**The device should have been registered at-least one time using eWelink app to obtain the device ID**
+
+
+# Usage
+**Requires Python 3.5 or above to be installed.**
+
+1. Clone the repository to your local machine using 
+		`git clone https://github.com/Philiptpp/sonoff_registered_devices.git`
+2. Navigate to the cloned directory using `cd sonoff_registered_devices`	
+3. Install all required pip dependencies using `pip install -r requirements.txt`
+4. Run the script using `python sonoffDevices.py`
+5. Enter the registered email & password used in the eWelink app. 
+
+## Output
+The script will list all registered devices for the given eWelink credentials.
+![Screenshot showing the output of the script](https://raw.githubusercontent.com/Philiptpp/sonoff_registered_devices/master/example/output.jpg)
+
+# Credits
+
+A vast portion of the code has been adapted from the amazing work done by [@peterbuga](https://github.com/peterbuga) in breaking down the api calls used by sonoff devices with coolkit api.
+
+[[https://github.com/peterbuga/HASS-sonoff-ewelink]](https://github.com/peterbuga/HASS-sonoff-ewelink)
---
+++ sonoffDevices.py
@@ -0,0 +1,92 @@
+import time, hmac, hashlib, random, base64, json, requests, uuid
+from http import HTTPStatus
+
+
+def create_signature(credentials):
+    app_details = {
+        'email': credentials['email'],
+        'password': credentials['password'],
+        'version': '6',
+        'ts': int(time.time()),
+        'nonce': ''.join([str(random.randint(0, 9)) for i in range(15)]),
+        'appid': 'oeVkj2lYFGnJu5XUtWisfW4utiN4u9Mq',
+        'imei': str(uuid.uuid4()),
+        'os': 'iOS',
+        'model': 'iPhone10,6',
+        'romVersion': '11.1.2',
+        'appVersion': '3.5.3'
+    }
+    decryptedAppSecret = b'6Nz4n0xA8s8qdxQf2GqurZj2Fs55FUvM'
+    hex_dig = hmac.new(
+        decryptedAppSecret,
+        str.encode(json.dumps(app_details)),
+        digestmod=hashlib.sha256).digest()
+    sign = base64.b64encode(hex_dig).decode()
+    return (sign, app_details)
+
+
+def login(credentials, api_region='us'):
+    sign, payload = create_signature(credentials)
+    headers = {
+        'Authorization' : 'Sign ' + sign,
+        'Content-Type'  : 'application/json;charset=UTF-8'
+    }
+
+    r = requests.post('https://{}-api.coolkit.cc:8080/api/user/login'.format(api_region),
+        headers=headers, json=payload)
+
+    if not (r.status_code == HTTPStatus.OK):
+        return ({"error": "Unable to access coolkit api server [{}]".format(r.text)})
+
+    resp = r.json()
+    if 'error' in resp and 'region' in resp and resp['error'] == HTTPStatus.MOVED_PERMANENTLY:
+        api_region = resp['region']
+        print('API region set to: {}'.format(api_region))
+        return login(credentials, api_region)
+
+    return resp
+
+
+def list_devices(user_info, attempt=1):
+    headers = {
+        'Authorization' : 'Bearer ' + user_info['at'],
+        'Content-Type'  : 'application/json;charset=UTF-8'
+    }
+
+    r = requests.get('https://{}-api.coolkit.cc:8080/api/user/device?lang=en&apiKey={}&getTags=1'.format(user_info['region'], user_info['user']['apikey']),
+            headers=headers)
+
+    resp = r.json()
+    if 'error' in resp and resp['error'] in [HTTPStatus.BAD_REQUEST, HTTPStatus.UNAUTHORIZED]:
+        if (attempt == 5):
+            print("Unable to fetch devices, please close eWelink application across all devices and try again.")
+            return None
+        return list_devices(user_info, attempt+1)
+
+    return resp
+
+
+if __name__ == '__main__':
+    print('eWelink Credentials')
+    print('-------------------')
+    email = input("eWelink registered email: ")
+    password = input("eWelink password: ")
+    print('\n')
+
+    user_info = login({
+        'email': email,
+        'password': password
+        })
+    
+    if 'at' not in user_info:
+        print("Login failed! Please check credentials!")
+
+    else:
+        devices = list_devices(user_info)
+        print('Found {} devices registered to this account'.format(len(devices)))
+        print('\nAPI Key: {}\n'.format(user_info['user']['apikey']))
+        if len(devices):
+            print('{:10} {:10} : {:40} [{}]'.format('Brand', 'Model', 'Device Name', 'Device ID'))
+            print('{:10} {:10} : {:40}  {}'.format('=====', '=====', '===========', '========='))
+            for device in devices:
+                print('{:10} {:10} : {:40} [{}]'.format(device['brandName'], device['productModel'], device['name'], device['deviceid']))
